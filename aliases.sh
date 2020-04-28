d4ac40b8dcdb23012705f752d29fdf8488bcbb58
Bhavya Gupta <61771399+Bhavya2612@users.noreply.github.com>
2020-04-28 19:17:34 +0530

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -10,6 +10,10 @@ echo alias attendance='./attendance.sh' >> ~/.bashrc
 
 echo alias record='./record.sh' >> ~/.bashrc
 
+echo alias finalattendance='./finalattendance.sh' >> ~/.bashrc
+
+echo alias nearest='./nearest.sh' >> ~/.bashrc
+
 
 
 echo All Aliases Have Been Created!
